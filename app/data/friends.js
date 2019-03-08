1221dead16fa783bf7c4d217e7c41eeed390f6a2
lydmgem <lydmgem@gmail.com>
2019-03-07 22:47:21 -0600

    Finalizing

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,16 +4,65 @@ var friends = [
     {
         name: "Sailor Moon",
         photo: "https://dw9to29mmj727.cloudfront.net/properties/2016/432-SeriesThumbnails_SM__400x320.jpg",
-        scores: [1,2,3,4,5,5,4,3,2,1]
+        scores: [
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
     }, {
         name: "Tuxedo Mask",
         photo: "https://i.ytimg.com/vi/1dCQs-hk9Jg/maxresdefault.jpg",
-        scores: [2,4,5,3,1,2,4,5,3,1,2]
+        scores: [
+            "2",
+            "4",
+            "5",
+            "3",
+            "1",
+            "2",
+            "4",
+            "5",
+            "3",
+            "1",
+            "2"
+        ]
     }, {
         name: "Hello Kitty",
         photo: "https://media1.tenor.com/images/b2f01ca41fafc7f1267887c81eeca1a6/tenor.gif?itemid=8672464",
-        scores: [5,2,4,1,3,2,4,1,5,5]
-    }
+        scores: [
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "4",
+            "1",
+            "5",
+            "5"
+        ]
+    }, {
+        name: "Lydia M.",
+        photo: "https://scontent.fhou1-1.fna.fbcdn.net/v/t1.0-9/53618357_10217946647080828_3815736060588064768_o.jpg?_nc_cat=104&_nc_ht=scontent.fhou1-1.fna&oh=1654b2f52fb70f9d36222a1f5e338995&oe=5CE2C338",
+        scores: [
+            "5",
+            "3",
+            "1",
+            "4",
+            "2",
+            "3",
+            "4",
+            "3",
+            "4",
+            "5"
+        ]
+    } 
 ];
 
 module.exports = friends;
